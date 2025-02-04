cd26df6c41375c23f12568b2f77d3ef8756d72c8
Julien Castets <castets.j@gmail.com>
2025-02-04 15:02:54 +0100

    linter: http backend test: remove ineffectual assignment

--- storage/backends/http/client_test.go
+++ storage/backends/http/client_test.go
@@ -287,7 +287,7 @@ func TestHttpBackend(t *testing.T) {
 	}
 	require.Equal(t, expected, packfiles)
 
-	rd, err = repo.GetPackfileBlob(checksum4, 0, 4)
+	rd, _ = repo.GetPackfileBlob(checksum4, 0, 4)
 	buf = new(bytes.Buffer)
 	_, err = io.Copy(buf, rd)
 	require.NoError(t, err)
@@ -301,7 +301,7 @@ func TestHttpBackend(t *testing.T) {
 	expected = []objects.Checksum{{0x60, 0x70, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}}
 	require.Equal(t, expected, packfiles)
 
-	rd, err = repo.GetPackfile(checksum4)
+	rd, _ = repo.GetPackfile(checksum4)
 	buf = new(bytes.Buffer)
 	_, err = io.Copy(buf, rd)
 	require.NoError(t, err)
